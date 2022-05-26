2887bf027f06c985a8db6805cf2a549cdfa782ac
Amir Hossein Hassanzade Moghadam <104617956+AmirHosseinHM@users.noreply.github.com>
2022-05-26 04:48:48 +0430

    Update mini_project.py

--- mini_project.py
+++ mini_project.py
@@ -5,14 +5,30 @@ from sympy import *
 class Vector:
     """This class performs some of the vector operations.
 
+    Calculating directional derivative is the most important thing this class does.
+    To use a directional derivative ,first of all you have to use \"directDervation_activator()\" method 
+    to assign symbols to the  x  and  y  variables. then you can define your ideal function.
+
+    Example:
+        z = Vector(3, 4, 5, 8)
+        x , y = z.directDervation_activator()
+        Function = x*y+y**2+y**3
+        print(z.directionalDervation(Function, x: xSymbol, y: ySymbol , xPoint(for example: 0) , yPoint(for example: 2)))
+        Result: 15.14
+
+    Attribute:
+    directDervation_key (bool): Used to control the \"directionalDervation()\" method
+
     Methods:
-           length(): Computes the length of the vector.
-           summation(v): Computes the summation of two vectors.
-           innerProduct(v): Computes the internal multiplication of two vectors.
-           toUniteVector(): Convert the vector to unit vector.
-           toPolarCoordinates(): Convert the coordinates of the vector from cartesian to polar.
-           directionalDerivation(F, x, y): Directional derivative of F at the point(x,y) in direction of the vector.
+        length(): Computes the length of the vector.
+        summation(v): Computes the summation of two vectors.
+        innerProduct(v): Computes the internal multiplication of two vectors.
+        toUniteVector(): Convert the vector to unit vector.
+        toPolarCoordinates(): Convert the coordinates of the vector from cartesian to polar.
+        directionalDerivation(F, x, y): Directional derivative of F at the point(x,y) in direction of the vector.
     """
+
+    directDervation_key = False
 
     def __init__(self, x0, y0, x1, y1):
         self.x0 = x0
@@ -39,7 +55,12 @@ class Vector:
         r = math.sqrt((vec[0]**2 + vec[1]**2))
         return [format(vec[0]/r, ".2f"), format(vec[1]/r, ".2f")] if activator else (f"{vec[0]}i/{r} + {vec[1]}j/{r}")
 
-    def toPolarCoordinates(self):
+    def toPolarCoordinates(self) -> tuple[float, float]:
+        """Convert the coordinates of the vector from cartesian to polar.
+
+        Returns:
+            tuple[float , float]: A tuple that have polarcoordinates.
+        """
         vec = Vector.length(self)
         r = math.sqrt((vec[0]**2 + vec[1]**2))
         O = math.degrees(math.atan(vec[1]/vec[0]))
@@ -53,23 +74,47 @@ class Vector:
         extracted_divY = eval(div_y)
         return extracted_divX, extracted_divY
 
-    def directionalDervation(self, F, x, y):
-        xSym, ySym = symbols('x y', real=True)
-        d1_x = diff(F, xSym)
-        d1_y = diff(F, ySym)
-        grad1, grad2 = Vector.extraction(str(d1_x), str(d1_y), x, y)
-        unitList = Vector.toUnitVector(self, True)
-        holder = [int(grad1), int(grad2), unitList]
-        return Vector.innerProduct(self, holder, True)
+    @classmethod
+    def directDervation_activator(cls, x="x", y="y"):
+        cls.directDervation_key = True
+        return symbols(x + " " + y, real=True)
+
+    def directionalDervation(self, F: core.Add, sym_x: core.symbol, sym_y: core.symbol, a: int, b: int) -> float:
+        """This method Calculates Directional derivative of F at the point(x,y) in direction of the vector.
+
+        To calculate Directional derivative, 3 parts must be considered:
+        _calculates Partial derivative of the function.
+        _calculates unit vector of (x , y)
+        _calculates innerProduct of two vector
+
+        Args:
+            F (sympy.core.Add): A function that we use to calculate Partial derivative
+            sym_x (sympy.core.symbol): x variable of function
+            sym_y (sympy.core.symbol): y variable of function
+            a (int): Coordinates of point x
+            b (int): Coordinates of point y
+
+        Returns:
+            float: Directional derivative
+        """
+        if Vector.directDervation_key:
+            d1_x = diff(F, sym_x)
+            d1_y = diff(F, sym_y)
+            grad1, grad2 = Vector.extraction(str(d1_x), str(d1_y), a, b)
+            unitList = Vector.toUnitVector(self, True)
+            holder = [int(grad1), int(grad2), unitList]
+            return Vector.innerProduct(self, holder, True)
+        else:
+            print("You must first use the \"directDervation_activator()\" method to assign symbols to the  x  and  y  variables")
 
 
 class stringRedefine:
     """This class performs some kind of operations on strings.
 
     Methods:
-           switchHalf(): Switch the place of the first half of string with the second part.
-           encryption(): This method encrypts a string.
-           primeASCII(): Performs some operations on the characters of a string.
+        switchHalf(): Switch the place of the first half of string with the second part.
+        encryption(): This method encrypts a string.
+        primeASCII(): Performs some operations on the characters of a string.
     """
 
     def __init__(self, s):
